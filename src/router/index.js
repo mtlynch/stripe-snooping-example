016bc18db662343ee74c5509df8b188772841f2d
Michael Lynch <git@mtlynch.io>
2020-04-14 17:46:53 -0400

    Changing router mode to history

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import About from '../views/About.vue'
 import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
@@ -7,21 +8,17 @@ Vue.use(VueRouter)
   const routes = [
   {
     path: '/',
-    name: 'Home',
     component: Home
   },
   {
     path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: About
   }
 ]
 
 const router = new VueRouter({
-  routes
+  routes,
+  mode: 'history'
 })
 
 export default router
